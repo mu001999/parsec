695a926a6075765d90dc7ccad2820364e3727070
mu001999 <mu001999@outlook.com>
2020-03-15 16:21:25 +0800

    init step 1 2

--- src/parsec.hpp
+++ src/parsec.hpp
@@ -10,18 +10,22 @@
 #include <type_traits>
 
 namespace parsec {
+template<typename T> struct IsTuple {
+    static constexpr bool value = false;
+};
+template<typename ...Ts> struct IsTuple<std::tuple<Ts...>> {
+    static constexpr bool value = true;
+};
+template<typename T> constexpr bool IsTuple_v = IsTuple<T>::value;
+
 template<typename T1, typename T2> struct Concat;
 template<typename T1, typename T2> struct Addition;
+template<typename T1, typename T2> struct Product;
 
 template<typename T1, typename T2> using Concat_t = typename Concat<T1, T2>::type;
 template<typename T1, typename T2> using Addition_t = typename Addition<T1, T2>::type;
+template<typename T1, typename T2> using Product_t = typename Product<T1, T2>::type;
 
-template<typename T1, typename ...T2s> struct Concat<T1, std::tuple<T2s...>> {
-    using type = std::tuple<T1, T2s...>;
-};
-template<typename ...T1s, typename ...T2s> struct Concat<std::tuple<T1s...>, std::tuple<T2s...>> {
-    using type = std::tuple<T1s..., T2s...>;
-};
 template<typename T1, typename ...T2s> struct Concat<T1, std::variant<T2s...>> {
     using type = std::variant<T1, T2s...>;
 };
@@ -42,7 +46,7 @@ template<typename T1, typename ...T1s, typename T2> struct Addition<std::variant
     using type = std::conditional_t<std::is_same_v<T1, T2>, std::variant<T1, T1s...>, Concat_t<T1, Addition_t<std::variant<T1s...>, T2>>>;
 };
 template<typename T1, typename T2, typename ...T2s> struct Addition<T1, std::variant<T2, T2s...>> {
-    using type = std::conditional_t<std::is_same_v<T1, T2>, std::variant<T2, T2s...>, Concat_t<T2, Addition_t<std::variant<T2s>..., T1>>>;
+    using type = std::conditional_t<std::is_same_v<T1, T2>, std::variant<T2, T2s...>, Concat_t<T2, Addition_t<std::variant<T2s...>, T1>>>;
 };
 template<typename T1, typename T2> struct Addition<std::variant<T1>, std::variant<T2>> {
     using type = std::conditional_t<std::is_same_v<T1, T2>, std::variant<T1>, std::variant<T1, T2>>;
@@ -54,10 +58,111 @@ template<typename T1, typename ...T1s, typename ...T2s> struct Addition<std::var
     using type = Concat_t<Addition_t<T1, std::variant<T2s...>>, Addition_t<std::variant<T1s...>, std::variant<T2s...>>>;
 };
 
+template<typename T1, typename T2> struct Product {
+    using type = std::tuple<T1, T2>;
+};
+template<typename ...T1s, typename T2> struct Product<std::tuple<T1s...>, T2> {
+    using type = std::tuple<T1s..., T2>;
+};
+template<typename T1, typename ...T2s> struct Product<T1, std::tuple<T2s...>> {
+    using type = std::tuple<T1, T2s...>;
+};
+template<typename ...T1s, typename ...T2s> struct Product<std::tuple<T1s...>, std::tuple<T2s...>> {
+    using type = std::tuple<T1s..., T2s...>;
+};
+template<typename T1, typename T2> struct Product<std::variant<T1>, T2> {
+    using type = std::variant<std::tuple<T1, T2>>;
+};
+template<typename T1, typename T2> struct Product<T1, std::variant<T2>> {
+    using type = std::variant<std::tuple<T1, T2>>;
+};
+template<typename T1, typename ...T1s, typename T2> struct Product<std::variant<T1, T1s...>, T2> {
+    using type = Concat_t<Product_t<T1, T2>, Product_t<std::variant<T1s...>, T2>>;
+};
+template<typename T1, typename T2, typename ...T2s> struct Product<T1, std::variant<T2, T2s...>> {
+    using type = Concat_t<Product_t<T1, T2>, Product_t<T1, std::variant<T2s...>>>;
+};
+template<typename T1, typename ...T2s> struct Product<std::variant<T1>, std::variant<T2s...>> {
+    using type = Product_t<T1, std::variant<T2s...>>;
+};
+template<typename T1, typename ...T1s, typename ...T2s> struct Product<std::variant<T1, T1s...>, std::variant<T2s...>> {
+    using type = Addition_t<Product_t<T1, std::variant<T2s...>>, Product_t<std::variant<T1s...>, std::variant<T2s...>>>;
+};
+
 class ParsecComponentBase {
   public:
     virtual ~ParsecComponentBase() = default;
 };
+
+template<typename Result>
+class ParsecComponent;
+
+template<typename Result>
+class Parsec;
+
+template<typename Result, typename Func1, typename Func2>
+inline std::optional<Result> callback_template(Func1 exec, Func2 callback, const std::string &str, std::size_t &index) {
+    auto anchor = index;
+    auto result = exec(str, index);
+    if (result) {
+        auto v = result.value();
+        if constexpr (IsTuple_v<decltype(v)>) {
+            return std::apply(callback, v);
+        } else {
+            return callback(v);
+        }
+    } else {
+        index = anchor;
+        return {};
+    }
+}
+
+template<typename Result, typename Func1, typename Func2>
+inline std::optional<Result> alternate_template(Func1 lexec, Func2 rexec, const std::string &str, std::size_t &index) {
+    auto anchor = index;
+    auto lr = lexec(str, index);
+    if (lr) {
+        return lr;
+    } else {
+        index = anchor;
+        auto rr = rexec(str, index);
+        if (rr) {
+            return rr;
+        } else {
+            index = anchor;
+            return {};
+        }
+    }
+}
+
+template<typename Result, typename Func1, typename Func2>
+inline std::optional<Result> connect_template(Func1 lexec, Func2 rexec, const std::string &str, std::size_t &index) {
+    auto anchor = index;
+    auto lr = lexec(str, index);
+    if (lr) {
+        anchor = index;
+        auto rr = rexec(str, index);
+        if (rr) {
+            auto lv = lr.value();
+            auto rv = rr.value();
+            if constexpr (IsTuple_v<decltype(lv)> and IsTuple_v<decltype(rv)>) {
+                return std::tuple_cat(lv, rv);
+            } else if constexpr (IsTuple_v<decltype(lv)>) {
+                return std::tuple_cat(lv, std::make_tuple(rv));
+            } else if constexpr (IsTuple_v<decltype(rv)>) {
+                return std::tuple_cat(std::make_tuple(lv), rv);
+            } else {
+                return std::make_tuple(lv, rv);
+            };
+        } else {
+            index = anchor;
+            return {};
+        }
+    } else {
+        index = anchor;
+        return {};
+    }
+}
 
 // Result must be a single type, Tuple or Variant
 template<typename Result>
@@ -67,80 +172,183 @@ class ParsecComponent : public ParsecComponentBase {
 
     virtual ~ParsecComponent() = default;
 
-    ResultType operator()(const std::string &str, std::size_t &index) {
+    ResultType operator()(const std::string &str, std::size_t &index) const {
         return exec_(str, index);
     }
 
+    // R maybe std::variant, type of Args also may be std::variant
+    template<typename Func>
+    auto operator>>(Func &&callback) const {
+        std::function cb = std::move(callback);
+        using NewResult = typename decltype(cb)::result_type;
+        ParsecComponent<NewResult> component;
+        component.set_exec([exec = this->exec(), cb](const std::string &str, std::size_t &index) {
+            return callback_template<NewResult>(exec, cb, str, index);
+        });
+        return component;
+    }
+
+    template<typename RhsResult,
+        typename NewResult = Addition_t<Result, RhsResult>>
+    ParsecComponent<NewResult> operator|(const ParsecComponent<RhsResult> &rhs) const {
+        ParsecComponent<NewResult> component;
+        component.set_exec([lexec = this->exec(), rexec = rhs.exec()](const std::string &str, std::size_t &index) {
+            return alternate_template<NewResult>(lexec, rexec, str, index);
+        });
+        return component;
+    }
+
+    template<typename RhsResult,
+        typename NewResult = Product_t<Result, RhsResult>>
+    ParsecComponent<NewResult> operator+(const ParsecComponent<RhsResult> &rhs) const {
+        ParsecComponent<NewResult> component;
+        component.set_exec([lexec = this->exec(), rexec = rhs.exec()](const std::string &str, std::size_t &index) {
+            return connect_template(lexec, rexec, str, index);
+        });
+        return component;
+    }
+
+    template<typename RhsResult>
+    ParsecComponent<Addition_t<Result, RhsResult>> operator|(const Parsec<RhsResult> &rhs) const;
+
+    template<typename RhsResult>
+    ParsecComponent<Product_t<Result, RhsResult>> operator+(const Parsec<RhsResult> &rhs) const;
+
+    void set_exec(std::function<ResultType(const std::string &, std::size_t &)> &&exec) {
+        exec_ = std::move(exec);
+    }
+
+    std::function<ResultType(const std::string &, std::size_t &)> exec() const {
+        return exec_;
+    }
+
+    friend ParsecComponent<char> operator""_T(char ch);
+
   private:
-    std::function<ResultType(const std::string &str, std::size_t &index)> exec_;
-
-    template<typename R, typename ...Args>
-    ParsecComponent operator>>(std::function<R(Args...)> callback) {
-        ParsecComponent<R> component;
-        component->exec_ = [callback](const std::string &str, std::size_t &index) -> std::optional<R> {
-            auto result = this->operator()(str, index);
-            if (result) {
-                return std::apply(callback, result.value());
-            } else {
-                return {};
-            }
-        };
-        return component;
-    }
-
-    template<typename RhsResult>
-    ParsecComponentBase operator+(ParsecComponent<RhsResult> &rhs) {
-        using NewResult = Product_t<Result, RhsResult>;
-        ParsecComponent<NewResult> component;
-        component.exec_ = [this, rhs](const std::string &str, std::size_t &index) -> std::optional<NewResult> {
-            auto lr = this->operator()(str, index);
-            if (lr) {
-                auto rr = rhs.operator()(str, index);
-                if (rr) {
-                    return std::tuple_cat(lr.value(), rr.value());
-                } else {
-                    return {};
-                }
-            } else {
-                return {};
-            }
-        };
-        return component;
-    }
-
-    template<typename RhsResult>
-    ParsecComponentBase operator|(ParsecComponent<RhsResult> &rhs) {
-        using NewResult = Addition_t<Result, RhsResult>;
-        ParsecComponent<NewResult> component;
-        component.exec_ = [this, rhs](const std::string &str, std::size_t &index) -> std::optional<NewResult> {
-            auto lr = this->operator()(str, index);
-            if (lr) {
-                return lr;
-            } else {
-                return rhs.operator()(str, index);
-            }
-        };
-        return component;
-    }
+    std::function<ResultType(const std::string &, std::size_t &)> exec_;
 };
 
 template<typename Result>
-class Parsec final {
+class Parsec {
   public:
     Parsec() = default;
 
-    Result operator()(const std::string &str, std::size_t &index) {
+    ParsecComponent<Result> *component() const {
+        return static_cast<ParsecComponent<Result>*>(component_);
+    }
+
+    Parsec &operator=(const ParsecComponent<Result> &component) {
+        component_ = new ParsecComponent<Result>(component);
+        return *this;
+    }
+
+    Result operator()(const std::string &str) const {
+        std::size_t index = 0;
+        return operator()(str, index);
+    }
+
+    Result operator()(const std::string &str, std::size_t &index) const {
         if (component_) {
-            auto r = static_cast<ParsecComponent<Result>*>(component_)->operator()(str, index);
+            auto r = component()->operator()(str, index);
             if (r) {
-                return r;
+                return r.value();
             } else {
                 throw std::runtime_error("parse error at " + std::to_string(index));
             }
+        } else {
+            throw std::runtime_error("not give definition");
         }
     }
 
+    template<typename Func>
+    auto operator>>(Func &&callback) {
+        std::function cb = std::move(callback);
+        using NewResult = typename decltype(cb)::result_type;
+        ParsecComponent<NewResult> component;
+        component.set_exec([this, cb](const std::string &str, std::size_t &index) {
+            return callback_template<NewResult>(this->component()->exec(), cb, str, index);
+        });
+        return component;
+    }
+
+    template<typename RhsResult,
+        typename NewResult = Addition_t<Result, RhsResult>>
+    ParsecComponent<NewResult> operator|(const Parsec<RhsResult> &rhs) {
+        ParsecComponent<NewResult> component;
+        component.set_exec([this, &rhs](const std::string &str, std::size_t &index) {
+            return alternate_template<NewResult>(this->component()->exec(), rhs.component()->exec(), str, index);
+        });
+        return component;
+    }
+
+    template<typename RhsResult,
+        typename NewResult = Product_t<Result, RhsResult>>
+    ParsecComponent<NewResult> operator+(const Parsec<RhsResult> &rhs) {
+        ParsecComponent<NewResult> component;
+        component.set_exec([this, &rhs](const std::string &str, std::size_t &index) {
+            return connect_template<NewResult>(this->component()->exec(), rhs.component()->exec(), str, index);
+        });
+        return component;
+    }
+
+    template<typename RhsResult,
+        typename NewResult = Addition_t<Result, RhsResult>>
+    ParsecComponent<NewResult> operator|(const ParsecComponent<RhsResult> &rhs) {
+        ParsecComponent<NewResult> component;
+        component.set_exec([this, rexec = rhs.exec()](const std::string &str, std::size_t &index) {
+            return alternate_template<NewResult>(this->component()->exec(), rexec, str, index);
+        });
+        return component;
+    }
+
+    template<typename RhsResult,
+        typename NewResult = Product_t<Result, RhsResult>>
+    ParsecComponent<NewResult> operator+(const ParsecComponent<RhsResult> &rhs) {
+        ParsecComponent<NewResult> component;
+        component.set_exec([this, rexec = rhs.exec()](const std::string &str, std::size_t &index) {
+            return connect_template<NewResult>(this->component()->exec(), rexec, str, index);
+        });
+        return component;
+    }
+
   private:
-    ParsecComponentBase *component_;
+    ParsecComponentBase *component_ = nullptr;
 };
+
+template<typename Result>
+template<typename RhsResult>
+inline ParsecComponent<Addition_t<Result, RhsResult>>
+ParsecComponent<Result>::operator|(const Parsec<RhsResult> &rhs) const {
+    using NewResult = Addition_t<Result, RhsResult>;
+    ParsecComponent<NewResult> component;
+    component.set_exec([lexec = this->exec(), &rhs](const std::string &str, std::size_t &index) {
+        return alternate_template<NewResult>(lexec, rhs.component()->exec(), str, index);
+    });
+    return component;
+}
+
+template<typename Result>
+template<typename RhsResult>
+inline ParsecComponent<Product_t<Result, RhsResult>>
+ParsecComponent<Result>::operator+(const Parsec<RhsResult> &rhs) const {
+    using NewResult = Product_t<Result, RhsResult>;
+    ParsecComponent<NewResult> component;
+    component.set_exec([lexec = this->exec(), &rhs](const std::string &str, std::size_t &index) {
+        return connect_template<NewResult>(lexec, rhs.component()->exec(), str, index);
+    });
+    return component;
+}
+
+inline ParsecComponent<char> operator""_T(char ch) {
+    ParsecComponent<char> component;
+    component.set_exec([ch](const std::string &str, std::size_t &length) -> std::optional<char> {
+        if (str[length] == ch) {
+            ++length;
+            return ch;
+        } else {
+            return {};
+        }
+    });
+    return component;
+}
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,50 @@
+#include <string>
+#include <iostream>
 #include "../src/parsec.hpp"
 
+using namespace std;
 using namespace parsec;
 
 int main(int argc, char *argv[])
 {
+    Parsec<char> Decimal;
+    Parsec<string> Number;
+    Parsec<int> Primary, Additive, Additive_;
+
+    // Decimal := '0' | ... | '9'
+    Decimal = ('0'_T | '1'_T | '2'_T | '3'_T | '4'_T | '5'_T | '6'_T | '7'_T | '8'_T | '9'_T );
+
+    // Number := Decimal Number | Decimal
+    Number =
+        (Decimal + Number >>
+            [](char decimal, string number) {
+                return decimal + number;
+            }) |
+        (Decimal >>
+            [](char decimal) {
+                return string() + decimal;
+            });
+
+    // Primary := Number
+    Primary = Number >> [](string number) {
+        return stoi(number);
+    };
+
+    // Additive := Primary Additive_ | Primary
+    // Additive_ := + Additive | - Additive
+    Additive =
+        (Primary + Additive_ >>
+            [](int primary, int additive) {
+                return primary + additive;
+            }) |
+        (Primary >> [](int primary) {
+            return primary;
+        });
+    Additive_ = (('+'_T | '-'_T) + Additive >> [](char op, int additive) {
+        return (op == '+' ? additive : -additive);
+    });
+
+    cout << Additive("1+2+3") << endl;
+
     return 0;
 }
