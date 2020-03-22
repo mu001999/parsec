a48635724c6519b5759f15ade6db2e59fdab6b65
mu001999 <mu001999@outlook.com>
2020-03-22 13:55:22 +0800

    use epsilon

--- test/test.cpp
+++ test/test.cpp
@@ -14,16 +14,13 @@ int main(int argc, char *argv[])
     // Decimal := '0' | ... | '9'
     Decimal = '0'_T | '1'_T | '2'_T | '3'_T | '4'_T | '5'_T | '6'_T | '7'_T | '8'_T | '9'_T;
 
-    // Number := Decimal Number | Decimal
+    // Number := Decimal Number | <epsilon>
     Number =
         Decimal + Number >>
             [](char decimal, string number) {
                 return decimal + number;
             } |
-        Decimal >>
-            [](char decimal) {
-                return string() + decimal;
-            };
+        Token::epsilon<string>();
 
     // Primary := Number
     Primary = Number >>
@@ -31,18 +28,16 @@ int main(int argc, char *argv[])
             return stoi(number);
         };
 
-    // Additive := Primary Additive_ | Primary
-    // Additive_ := + Additive | - Additive
-    Additive =
-        Primary + Additive_ >>
-            [](int primary, int additive) {
-                return primary + additive; } |
-        Primary >>
-            [](int primary) {
-                return primary; };
-    Additive_ = ('+'_T | '-'_T) + Additive >>
-        [](char op, int additive) {
-            return (op == '+' ? additive : -additive); };
+    // Additive := Primary Additive_
+    // Additive_ := + Additive | - Additive | <epsilon>
+    Additive = Primary + Additive_ >>
+        [](int primary, int additive) {
+            return primary + additive; };
+    Additive_ =
+        ('+'_T | '-'_T) + Additive >>
+            [](char op, int additive) {
+                return (op == '+' ? additive : -additive); } |
+        Token::epsilon<int>();
 
     cout << Additive("1+2+3") << endl;
 
